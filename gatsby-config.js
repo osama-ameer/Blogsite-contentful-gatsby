66c46140d6502f4638341b7edbbdd85b69f0bb20
Osama Ameer <osamaameer15@gmail.com>
2021-09-02 19:29:56 +0500

    updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,8 @@ module.exports = {
       options: {
         spaceId: "qli877ujsryw",
         accessToken: "C0ZAHbvswGGSyeq-5Rt9LPZuPqgPT4b0iqWaRcnUppM",
+        forceFullSync: true,
+
       }
     },
     `gatsby-transformer-sharp`,
